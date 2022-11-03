7fdcc90e012cee8296b7801283b4911405b4079c
JianLiu1994 <709501841@qq.com>
2022-11-03 10:54:38 -0500

    updated download scripts

--- NewsAggregatorDataset/workdir/test.py
+++ NewsAggregatorDataset/workdir/test.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 colnames=['ID', 'TITLE', 'URL', 'PUBLISHER', 'CATEGORY', 'STORY', 'HOSTNAME', 'TIMESTAMP'] 
 
-df = pd.read_csv('newsCorpora.csv', names=colnames, sep='\t')
+df = pd.read_csv('../newsCorpora.csv', names=colnames, sep='\t')
 
 import newspaper
 from newspaper import Article
@@ -11,19 +11,21 @@ from multiprocessing import Pool
 
 def download_url(inparams):
     url, category = inparams
-    print("Try to download from " + url)
+    # print("Try to download from " + url)
     try:
         article = Article(url)
         article.download()
         article.parse()
         if len(article.text) > 150 and len(article.text) < 6000:
             article.nlp()
+            print(article.text)
             return url, article.text, article.summary, category
     except Exception:
         return None, None, None, None
     return None, None, None, None
 
 process_list = []
+urls, contents, summarys, categorys = [], [], [], []
 counter = 1
 for i in range(len(df)):
     process_list.append([df.loc[i, 'URL'], df.loc[i, 'CATEGORY']])
@@ -33,12 +35,8 @@ for i in range(len(df)):
         pool.close()
         pool.join()
 
-        urls = []
-        contents = []
-        summarys = []
-        categorys = []
         for result in results:
-            url, content, summary, caregory = result
+            url, content, summary, category = result
             if url is not None:
                 urls += [url]
                 contents += [content]
@@ -47,6 +45,7 @@ for i in range(len(df)):
 
         new_df = pd.DataFrame({'url': urls, 'content': contents, 'summary': summarys, 'category': categorys})
         new_df.to_csv(f'dataset_{counter}.csv')
-
+        print(f"Saving dataset_{counter}.csv")
         process_list = []
+        urls, contents, summarys, categorys = [], [], [], []
         counter += 1
